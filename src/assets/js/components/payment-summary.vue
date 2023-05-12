89e4f7125da4502fddb94909b33355a95ce65715
Marc Roige <marc.roige@estudiants.urv.cat>
2023-05-13 01:56:54 +0200

    added return button

--- src/assets/js/components/payment-summary.vue
+++ src/assets/js/components/payment-summary.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
       <h1>Resum de la compra</h1>
-        InformaciÃ³ de l'ordre  {{this.order }} <br>
-        
+        Informacio de l'ordre:<br>
+        ID: {{ this.order.id }} <br>
+        Payer Email: {{ this.order.payerEmail }}<br>
+        First Name: {{ this.order.payerFirstName }}<br>
+        Last Name: {{ this.order.payerLastName }}<br>
+        Payer ID: {{ this.order.payerId }}<br>
+        Amount: {{ this.order.amount }}<br>
+        Currency: {{ this.order.currency }}<br>
+        Create Time: {{ this.order.createTime }}<br><br>
+
+        <router-link to="/" class="btn btn-primary">Tornar a la pàgina principal</router-link>
     </div>
 </template>
   
@@ -16,7 +25,6 @@
         mounted() {
             const order= this.$route.query.order;
             this.order = JSON.parse(order);
-            console.log(this.order);
         }
 }
 
